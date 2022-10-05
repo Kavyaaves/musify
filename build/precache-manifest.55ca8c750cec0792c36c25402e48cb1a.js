6d59c2fc480fe093206affc64f5c6fbf11ccbeab
kavya-murali-offl <kavyamuralioff@gmail.com>
2022-10-05 21:17:41 +0530

    Build

---
+++ build/precache-manifest.55ca8c750cec0792c36c25402e48cb1a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d682c3cb0853d469048624e7d86747",
+    "url": "/Kavyaaves/musify/index.html"
+  },
+  {
+    "revision": "68248b21379c2c365825",
+    "url": "/Kavyaaves/musify/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "398c31b33b3994629b39",
+    "url": "/Kavyaaves/musify/static/css/main.2b99ddeb.chunk.css"
+  },
+  {
+    "revision": "68248b21379c2c365825",
+    "url": "/Kavyaaves/musify/static/js/2.6be29c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kavyaaves/musify/static/js/2.6be29c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398c31b33b3994629b39",
+    "url": "/Kavyaaves/musify/static/js/main.bb2f7325.chunk.js"
+  },
+  {
+    "revision": "55c5eac03637eb0eb564",
+    "url": "/Kavyaaves/musify/static/js/runtime-main.f4f3eba2.js"
+  },
+  {
+    "revision": "290de989a964913166e1f25341dbbb47",
+    "url": "/Kavyaaves/musify/static/media/gifbg.290de989.gif"
+  },
+  {
+    "revision": "51e8a0f837c0dd01ef1079498bdef33b",
+    "url": "/Kavyaaves/musify/static/media/jslogo.51e8a0f8.png"
+  }
+]);
